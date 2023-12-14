f69cbeecb0efc8ec4b9ad28d4a15f07db301c2a8
Norman Pong <zhiming3399@gmail.com>
2023-12-14 18:22:41 +0800

    chore: Independent guard module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
   constructor() {}
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@Req() req: Request) {
-    console.log({ user: req.user });
-    return 'user info2';
+    return req.user;
   }
 }
